af2c346a2630fcc2b84aeb5c3f53aed6c4926746
A20ura <andrea.ventura23993@gmail.com>
2024-02-26 10:33:34 +0100

    first commit with sprg initializer project loaded

---
+++ src/main/java/BW5group5/GestioneClientiFatture/GestioneClientiFattureApplication.java
@@ -0,0 +1,13 @@
+package BW5group5.GestioneClientiFatture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneClientiFattureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneClientiFattureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BW5group5/GestioneClientiFatture/GestioneClientiFattureApplicationTests.java
@@ -0,0 +1,13 @@
+package BW5group5.GestioneClientiFatture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneClientiFattureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
